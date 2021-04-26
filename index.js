d4f685f22dd4cd753c5b6a194a846c977c3c5f27
jokatty <jyotikattani@gmail.com>
2021-04-26 15:34:46 +0800

    add base feature: hex to RGB conversion

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+
+const userInput = process.argv[2];
+
+// function converts hex to rgb
+// @param {string} hex - user input hex value.
+// returns rgb value
+  
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+let result = hexToRgb(userInput);
+console.log(`rgb(${result.r},${result.g},${result.b})`);
